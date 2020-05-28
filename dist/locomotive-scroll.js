1c684a95889754b64ebd726f15851688c52cf7e7
Jérémy Minié <jeremy.minie@gmail.com>
2020-05-28 14:47:19 -0400

    3.5.3

--- dist/locomotive-scroll.js
+++ dist/locomotive-scroll.js
@@ -1,4 +1,4 @@
-/* locomotive-scroll v3.5.2 | MIT License | https://github.com/locomotivemtl/locomotive-scroll */
+/* locomotive-scroll v3.5.3 | MIT License | https://github.com/locomotivemtl/locomotive-scroll */
 (function (global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
   typeof define === 'function' && define.amd ? define(factory) :
