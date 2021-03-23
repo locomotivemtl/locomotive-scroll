b6006c1cfe8f84538a7ce8307ff5e285c7a9e7ce
Jérémy Minié <jeremy.minie@gmail.com>
2021-03-23 16:52:06 -0400

    4.1.1

--- dist/locomotive-scroll.esm.js
+++ dist/locomotive-scroll.esm.js
@@ -1,4 +1,4 @@
-/* locomotive-scroll v4.1.0 | MIT License | https://github.com/locomotivemtl/locomotive-scroll */
+/* locomotive-scroll v4.1.1 | MIT License | https://github.com/locomotivemtl/locomotive-scroll */
 function _classCallCheck(instance, Constructor) {
   if (!(instance instanceof Constructor)) {
     throw new TypeError("Cannot call a class as a function");
