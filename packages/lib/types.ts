5abbc2a99a141a3d84a8e099e2a207815dfee8ab
Arnvvd <pinot.arnaud@gmail.com>
2026-01-14 12:24:28 -0500

    Add locomotiveScrollVersion to window for wappalyzer

--- packages/lib/types.ts
+++ packages/lib/types.ts
@@ -196,3 +196,9 @@ export interface CoreOptions {
 export type scrollOrientation = 'vertical' | 'horizontal';
 
 export type gestureOrientation = 'vertical' | 'horizontal' | 'both';
+
+declare global {
+    interface Window {
+        locomotiveScrollVersion: string;
+    }
+}
